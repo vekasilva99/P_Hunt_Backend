e53b7dc19f23cfd180e510f354c6ed07daa4e1ee
Veka Silva <vekasilva99@gmail.com>
2020-08-01 21:23:56 -0400

    a

--- graphql/resolvers/queries/products.js
+++ graphql/resolvers/queries/products.js
@@ -38,10 +38,7 @@ const user = async (userId) => {
 module.exports = {
   productsOrdered: async (_, args, context) => {
     return Product.find()
-      .sort([
-        ["createdAt", -1],
-        ["votes", 1],
-      ])
+      .sort({ votes: -1, createdAt: -1 })
       .then((products) => {
         return products.map((product) => {
           return {
